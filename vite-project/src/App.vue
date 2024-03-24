cf8324594e04f4ae7ae9997046a4b493dbc01abd
LeftRightHand <963292639@qq.com>
2024-03-24 20:12:54 +0800

    no message

---
+++ vite-project/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>SVG</h1>
+        <svg style="height: 30px; width: 30px;">
+            <use xlink-href="#icon-phone" fill="red"></use>
+        </svg>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
---
+++ vite-project/src/main.ts
@@ -0,0 +1,17 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+//@ts-ignore
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+
+const app = createApp(App)
+app.use(ElementPlus);
+app.use(ElementPlus, {
+    locale: zhCn
+});
+
+import 'virtual:svg-icons-register'
+
+app.mount("#app");
