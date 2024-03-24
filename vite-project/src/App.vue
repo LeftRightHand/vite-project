cbabd43ad5303427047119a19065c0a7d5210243
LeftRightHand <963292639@qq.com>
2024-03-24 20:30:36 +0800

    测试上传

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,9 +1,6 @@
 <template>
     <div>
-        <h1>SVG</h1>
-        <svg style="height: 30px; width: 30px;">
-            <use xlink-href="#icon-phone" fill="red"></use>
-        </svg>
+        <h1>Github代码上传</h1>
     </div>
 </template>
 
